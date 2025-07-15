ccabd26d2b4da33cfec88d4ffd7c6d64281824ec
CodeMat17 <codemat.biz@gmail.com>
2025-07-15 15:35:03 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { checkRole } from "@/utils/roles";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -17,22 +18,28 @@ export default async function HomePage() {
       <h1 className='text-4xl font-bold mb-3 mt-4 text-center'>
         NFVCB Cooperative Loan Manager <br /> (Admin Dashboard)
       </h1>
-      <p className='text-lg text-slate-600 mb-8 text-center'>
-       YOU MUST BE AN ADMIN TO HAVE ACCESS TO THE DASHBOARD
+      <p className='text-lg text-red-600 animate-bounce mb-8 text-center'>
+        YOU MUST BE AN ADMIN TO HAVE ACCESS TO THE DASHBOARD
       </p>
       <div className='space-y-4 flex flex-col items-center justify-center mt-12'>
-        <Button asChild>
-          <Link href='/sign-in' className=''>
-            Sign In
-          </Link>
-        </Button>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
-        <p className='text-sm text-slate-500'>
-          Don&apos;t have an account?{" "}
-          <Link href='/sign-up' className='text-slate-900 hover:underline'>
-            Sign Up
-          </Link>
-        </p>
+        <SignedOut>
+          <Button asChild>
+            <Link href='/sign-in' className=''>
+              Sign In
+            </Link>
+          </Button>
+
+          <p className='text-sm text-slate-500'>
+            Don&apos;t have an account?{" "}
+            <Link href='/sign-up' className='text-slate-900 hover:underline'>
+              Sign Up
+            </Link>
+          </p>
+        </SignedOut>
       </div>
     </div>
   );
